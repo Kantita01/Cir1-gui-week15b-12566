023cbd6adea6f2a3424d1386c22aab9aab4d5753
Kantita01 <kantita5069@gmail.com>
2023-08-25 09:33:30 +0700

    first commit

---
+++ bin/dart_week15b.dart
@@ -0,0 +1,12 @@
+import 'package:dart_week15b/map_demo.dart';
+// import 'package:dart_week15b/set_demo.dart';
+
+void main(){
+  protoType();
+}
+
+void protoType(){
+  mapDemo();
+  //setDemo();
+  //listDemo();
+}
---
+++ lib/map_demo.dart
@@ -0,0 +1,13 @@
+void mapDemo(){
+  muTable();
+}
+
+void muTable(){
+  Map<String,String> country = {
+    'Thai':'Bangkok',
+    'Japan':'Tokyo',
+    'Vietnam':'Hanoi'
+    };
+    print(country);
+    print(country['Thai']);
+}
